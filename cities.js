d8afcb80c0910064143b4dc3df1f4c9f2f55a653
Sreehari Annam <sannam@wescodist.com>
2026-07-02 09:56:36 -0400

    batch 3: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,8 @@
 const vermontCities = [
-  "Montgomery Center", "Montpelier", "Moretown", "Morrisville", "New Haven",
-  "Newfane", "Newport", "North Bennington", "North Clarendon", "North Concord",
-  "North Ferrisburgh", "North Hero", "North Pomfret", "North Pownal", "North Springfield",
-  "North Troy", "Northfield", "Norwich", "Orleans", "Orwell",
-  "Passumpsic", "Peacham", "Perkinsville", "Pittsford", "Plainfield",
-  "Post Mills", "Poultney", "Pownal", "Proctor", "Proctorsville",
-  "Putney", "Quechee", "Randolph", "Randolph Center", "Richford",
-  "Richmond", "Rochester", "Rupert", "Rutland", "Saint Albans",
-  "Saint Albans Bay", "Saint Johnsbury", "Shaftsbury", "Sharon", "Shelburne",
-  "Sheldon", "Shoreham", "South Barre", "South Burlington", "South Hero",
-  "South Royalton", "South Ryegate", "South Strafford", "Springfield", "Stamford",
-  "Starksboro", "Stowe", "Swanton", "Taftsville", "Thetford Center",
-  "Townshend", "Tunbridge", "Underhill", "Underhill Center", "Vergennes",
-  "Vernon", "Vershire", "Waitsfield", "Wallingford", "Warren",
-  "Washington", "Waterbury", "Waterbury Center", "Waterville", "Wells",
-  "West Burke", "West Dover", "West Dummerston", "West Newbury", "West Rutland",
+  "West Townshend", "Westford", "Westminster", "Westminster Station", "Weston",
+  "White River Junction", "Whiting", "Wilder", "Williamstown", "Williston",
+  "Wilmington", "Windsor", "Winooski", "Wolcott", "Woodstock",
+  "Worcester",
 ];
 
 const BATCH_SIZE = 5;
